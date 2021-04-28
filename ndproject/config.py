8737ff52e2dcf1919335de875e6936aa88509e20
Devill Don <53294521+devillD@users.noreply.github.com>
2021-04-28 15:27:21 +0600

    Create config.py

---
+++ ndproject/config.py
@@ -0,0 +1,10 @@
+import os
+# Bot Telegram API Token
+API_TOKEN = os.environ.get("BOT_TOKEN")
+
+# Bot Details
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_NAME = os.environ.get("BOT_NAME")
+
+# Models Path
+NUDITY_DETECTION_MODEL_PATH = os.environ.get("MODEL_PATH")
