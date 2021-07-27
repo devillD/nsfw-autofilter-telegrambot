cdd9ca43e4ce1b1f4ba63ca4be7c9789acce31cf
Devill Don <53294521+devillD@users.noreply.github.com>
2021-07-27 17:01:54 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py runserver
+python manage.py makemigrations && python manage.py migrate && python manage.py runserver
