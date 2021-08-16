9a38653b1b10e4efef5b165e14eb28b7db9ee27e
Devill Don <53294521+devillD@users.noreply.github.com>
2021-08-16 21:48:01 +0600

    Update config.py

--- ndproject/config.py
+++ ndproject/config.py
@@ -7,7 +7,7 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME")
 BOT_NAME = os.environ.get("BOT_NAME")
 
 # Models Path
-NUDITY_DETECTION_MODEL_PATH = "/BOT/model/classifier_model"
+NUDITY_DETECTION_MODEL_PATH = "/BOT/model/classifier_model.h5"
 
 # Image length
 IMAGE_LENGTH = os.environ.get("IMAGE_LENGTH")
